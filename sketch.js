b72e8603b2f23d467a1e2b772a76dafda0c7f29f
Dhriti-31 <78593550+Dhriti-31@users.noreply.github.com>
2021-05-17 10:27:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,27 +12,32 @@ function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
 
-
 	engine = Engine.create();
 	world = engine.world;
 	
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-	Paper = new paper(200,100,20);
+	Paper = new paper(300,100,20);
 
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(230);
- 
 
   groundObject.display();
   dustbinObj.display();
   Paper.display();
+  Paper.scale = 4;
 
+}
+
+function KeyPressed(){
+	if(keyCode === UP_ARROW){
+
+		Mattter.Body.applyForce(Paper.body, Paper.body.position,{x:130,y:145})
+	}
 }
 
